88c03c65fc4cc6bc6f94072b7a10de603f74c731
Resn4R <purview-joust.0x@icloud.com>
2023-11-26 17:36:22 +0000

    Initial Commit

---
+++ Hot Prospects/Hot_ProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hot_ProspectsApp.swift
+//  Hot Prospects
+//
+//  Created by Vito Borghi on 26/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Hot_ProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
