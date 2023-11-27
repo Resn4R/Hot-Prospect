b7dd3572164b7317dbc73e512e0388472edef798
Resn4R <purview-joust.0x@icloud.com>
2023-11-27 16:40:55 +0000

    exploring TabView()

--- Hot Prospects/ContentView.swift
+++ Hot Prospects/ContentView.swift
@@ -7,40 +7,23 @@
 
 import SwiftUI
 
-@MainActor class User: ObservableObject {
-    @Published var name = "Username123"
-}
-
-struct EditView: View {
-    @EnvironmentObject var user: User
-    
-    var body: some View {
-        TextField("name", text: $user.name)
-    }
-}
-
-struct DisplayView: View {
-    @EnvironmentObject var user: User
-
-    var body: some View {
-        VStack {
-            Image(systemName: "person")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text(user.name)
-        }
-        .padding()
-    }
-}
-
 struct ContentView: View {
-    @StateObject private var user = User()
+    @State private var selectedTab = "one"
     var body: some View {
-        VStack{
-            EditView()
-            DisplayView()
+        TabView {
+            Text("Tab 1")
+                .onTapGesture {
+                    selectedTab = "two"
+                }
+                .tabItem {
+                    Label("one", systemImage: "star")
+                }
+            Text("Tab 2")
+                .tabItem {
+                    Label("two", systemImage: "circle")
+                }
+                .tag("two")
         }
-        .environmentObject(user)
     }
 }
 
