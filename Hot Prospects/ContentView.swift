46c82837f8909521f9d0f7291c40b9fcf6ee7b8e
Resn4R <purview-joust.0x@icloud.com>
2023-11-28 18:51:37 +0000

    Creating context menus

--- Hot Prospects/ContentView.swift
+++ Hot Prospects/ContentView.swift
@@ -9,18 +9,43 @@ import SwiftUI
 
 
 struct ContentView: View {
-
+    @State private var backgroundColour = Color.red
+    @State private var chosenOption = 1
     var body: some View {
-        Image("example")
-            .interpolation(.none)
-            .resizable()
-            .scaledToFit()
-            .frame(maxHeight: .infinity)
-            .background(.black)
-            .ignoresSafeArea()
+        VStack {
+            Text("Hello, world!")
+                .padding()
+                .background(backgroundColour)
+            
+            Text("Change colour")
+                .padding()
+                .contextMenu {
+                    Button {
+                        backgroundColour = .red
+                        chosenOption = 1
+                    } label: {
+                        Label("Red", systemImage: chosenOption == 1 ?  "checkmark.circle.fill" : "")
+                    }
+                    .tag(1)
+                    
+                    Button{
+                        backgroundColour = .green
+                        chosenOption = 2
+                    } label: {
+                        Label("Green", systemImage: chosenOption == 2 ?  "checkmark.circle.fill" : "")
+                    }
+                    .tag(2)
+                    
+                    Button {
+                        backgroundColour = .blue
+                        chosenOption = 3
+                    } label: {
+                        Label("Blue", systemImage: chosenOption == 3 ?  "checkmark.circle.fill" : "")
+                    }
+                    .tag(3)
+                }
+        }
     }
-    
-
 }
 
 #Preview {
