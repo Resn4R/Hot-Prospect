390d3fd296b54de868d39b9bf622d81ec6338b8d
Resn4R <purview-joust.0x@icloud.com>
2023-11-28 18:42:04 +0000

    Controlling image interpolation in SwiftUI

--- Hot Prospects/ContentView.swift
+++ Hot Prospects/ContentView.swift
@@ -9,33 +9,18 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State private var selectedTab = "one"
-    
-    @State private var output = ""
+
     var body: some View {
-        Text(output)
-            .task {
-                await fetchReadings()
-            }
+        Image("example")
+            .interpolation(.none)
+            .resizable()
+            .scaledToFit()
+            .frame(maxHeight: .infinity)
+            .background(.black)
+            .ignoresSafeArea()
     }
     
-    func fetchReadings() async {
-        let fetchTask = Task { () -> String in
-            let url = URL(string: "https://hws.dev?readings.json")!
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let readings = try JSONDecoder().decode([Double].self, from: data)
-            return "Found \(readings.count) readings."
-        }
-        
-        let result = await fetchTask.result
-        
-        switch result {
-        case .success(let str):
-            output = str
-        case .failure(let error):
-            output = "dwnload error: \(error.localizedDescription)"
-        }
-    }
+
 }
 
 #Preview {
