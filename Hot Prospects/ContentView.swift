c16facb88c3b633fdbc9f323b741d4f8594df040
Resn4R <purview-joust.0x@icloud.com>
2023-11-28 19:13:33 +0000

    Scheduling local notifications

--- Hot Prospects/ContentView.swift
+++ Hot Prospects/ContentView.swift
@@ -6,32 +6,34 @@
 //
 
 import SwiftUI
-
+import UserNotifications
 
 struct ContentView: View {
     var body: some View {
-        List{
-            Text("Test List")
-                .swipeActions {
-                    Button{
-                        print("hello")
-                    } label: {
-                        Label("Send Message", systemImage: "message")
-                    }
-                    Button(role: .destructive) {
-                        print("deleting...")
-                    } label: {
-                        Label("Deleting", systemImage: "minus.circle")
+        VStack{
+            Button("Request Permision") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print ("all set")
+                    } else if let error = error {
+                        print(error.localizedDescription)
                     }
                 }
-                .swipeActions(edge: .leading) {
-                    Button {
-                        print("Pinning")
-                    } label: {
-                        Label("Pin", image: "pin")
-                    }
-                    .tint(.orange)
-                }
+            }
+            .padding()
+            
+            Button("Schedule Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "feed the dogs"
+                content.subtitle = "they look hungry"
+                content.sound = UNNotificationSound.default
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request)
+            }
         }
     }
 }
