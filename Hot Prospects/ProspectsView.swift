0d62c7563177f29615b4e7cb71859be9e8f193a2
Resn4R <purview-joust.0x@icloud.com>
2023-12-10 15:22:05 +0000

    Scanning QR codes with SwiftUI

--- Hot Prospects/ProspectsView.swift
+++ Hot Prospects/ProspectsView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Vito Borghi on 29/11/2023.
 //
-
+import CodeScanner
 import SwiftUI
 
 struct ProspectsView: View {
@@ -14,6 +14,7 @@ struct ProspectsView: View {
     }
     
     @EnvironmentObject var prospects: Prospects
+    @State private var isShowingScanner = false
     
     let filter: FilterType
     
@@ -52,15 +53,30 @@ struct ProspectsView: View {
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
                     Button {
-                        let prospect = Prospect()
-                        prospect.name = "Paul Hudson"
-                        prospect.emailAddress = "paul@hackingwithswift.com"
-                        prospects.people.append(prospect)
+                        isShowingScanner = true
                     } label: {
                         Label("Scan", systemImage: "qrcode.viewfinder")
                     }
                 }
             }
+            .sheet(isPresented: $isShowingScanner) {
+                CodeScannerView(codeTypes: [.qr], simulatedData: "Paul Hudson\npaul@hackingwithswift.com", completion: handleScan)
+            }
+        }
+    }
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        isShowingScanner = false
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: .newlines)
+            guard details.count == 2 else { return }
+            
+            let person = Prospect()
+            person.name = details[0]
+            person.emailAddress = details[1]
+            prospects.people.append(person)
+        case .failure(let error):
+            print("Scanning Failed: \(error.localizedDescription)")
         }
     }
 }
